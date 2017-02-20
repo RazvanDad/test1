59f4b222d6d5f2d28d3e9893fd7f6b0e1606e68d
Razvan Dad <razvan.dad@gmail.com>
2017-02-20 18:42:59 +0200

    new changes

--- FinalResult/rules.groovy
+++
@@ -1,8 +0,0 @@
-ruleset {
-    ruleset 'rulesets/basic.xml'
-    ruleset 'rulesets/braces.xml'
-    ruleset('rulesets/dry.xml') {
-        DuplicateNumberLiteral(enabled:false)
-    }
-    ruleset 'rulesets/groovyism.xml'
-}
